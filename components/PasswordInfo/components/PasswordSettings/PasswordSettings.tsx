446afdddfd77cac4023fc0e69e750ef3d46d9d5a
Ivan Dolgushin <iv.dolgushin@gmail.com>
2024-03-29 00:09:55 +0600

    feat: add password length changing

--- components/PasswordInfo/components/PasswordSettings/PasswordSettings.tsx
+++ components/PasswordInfo/components/PasswordSettings/PasswordSettings.tsx
@@ -3,19 +3,30 @@ import { Text, View, Switch, Button, StyleSheet } from 'react-native';
 import { SETTINGS_ITEMS } from './const/settings-items';
 import { SettingsItem } from './types/settings-item';
 import { generatePassword } from './utils/generate-password';
+import Slider from '@react-native-community/slider';
 
 type PasswordSettingsProps = {
   onChangePassword: (_newPassword: string) => void;
 };
 
+const MIN_PASSWORD_LENGTH = 6;
+const MAX_PASSWORD_LENGTH = 20;
+
+const SLIDER_STEP = 1;
+
 export const PasswordSettings: FC<PasswordSettingsProps> = ({
   onChangePassword,
 }) => {
+  const [passwordLength, setPasswordLength] =
+    useState<number>(MIN_PASSWORD_LENGTH);
   const [settings, setSettings] = useState<SettingsItem[]>(SETTINGS_ITEMS);
 
-  const isDisabled = useMemo(() => !settings.some(({isSelected}) => isSelected), [settings])
+  const isDisabled = useMemo(
+    () => !settings.some(({ isSelected }) => isSelected),
+    [settings]
+  );
 
-  const handleChangeSettings = useCallback(
+  const handleSettingsChanging = useCallback(
     (name: string) => {
       const newSettings = settings.map((item) =>
         item.label === name ? { ...item, isSelected: !item.isSelected } : item
@@ -34,30 +45,41 @@ export const PasswordSettings: FC<PasswordSettingsProps> = ({
         [] as string[]
       );
 
-    const newPassword = generatePassword(characters, 10);
+    const newPassword = generatePassword(characters, passwordLength);
 
     onChangePassword(newPassword);
-  }, [settings, onChangePassword]);
+  }, [settings, passwordLength, onChangePassword]);
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text>Characters length</Text>
-        <Text>10</Text>
+        <Text>{passwordLength}</Text>
       </View>
+      <Slider
+        step={SLIDER_STEP}
+        value={passwordLength}
+        minimumValue={MIN_PASSWORD_LENGTH}
+        maximumValue={MAX_PASSWORD_LENGTH}
+        onValueChange={setPasswordLength}
+      />
       <View style={styles.actions}>
         {settings.map(({ label, isSelected }) => (
           <Switch
             key={label}
             value={isSelected}
-            onChange={() => handleChangeSettings(label)}
+            onChange={() => handleSettingsChanging(label)}
           />
         ))}
       </View>
       <View>
         <Text>Strength</Text>
       </View>
-      <Button title="generate" disabled={isDisabled} onPress={handlePasswordGenerating} />
+      <Button
+        title="generate"
+        disabled={isDisabled}
+        onPress={handlePasswordGenerating}
+      />
     </View>
   );
 };
